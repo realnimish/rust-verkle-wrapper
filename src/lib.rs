a4232a3c311f4c7cccc66a728217ddbb031eb6b5
Parinay Chauhan <parinayc20@iitk.ac.in>
2022-01-13 20:09:40 +0530

    More functions added

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,28 @@
+#![feature(vec_into_raw_parts)]
 use std::ptr;
 use std::mem::transmute;
+use std::mem;
 use std::slice;
-
-extern crate verkle_trie;
 
 use ark_ec::ProjectiveCurve;
 use once_cell::sync::Lazy;
 use std::convert::TryInto;
-use verkle_db::{BareMetalDiskDb};
-use verkle_trie::{database::{memory_db::MemoryDb, VerkleDb}, trie::Trie, Value};
+use verkle_trie::{database::{memory_db::MemoryDb}, trie::Trie};
 use verkle_trie::committer::precompute::PrecomputeLagrange;
 use verkle_trie::config::{VerkleConfig};
-use verkle_trie::database::ReadWriteHigherDb;
 use verkle_trie::TrieTrait;
+use verkle_trie::proof::VerkleProof;
 
 #[repr(C)]
 pub struct VerkleTrie {
     trie: Trie<MemoryDb, PrecomputeLagrange>,
 }
 
+#[repr(C)]
+pub struct Proof {
+    pub ptr: *const u8,
+    pub len: usize,
+} 
 
 pub extern fn get_verkle_trie() -> Trie<MemoryDb, PrecomputeLagrange> {
     let _db = MemoryDb::new();
@@ -59,43 +63,86 @@ pub extern fn verkle_trie_insert(vt: *mut VerkleTrie, key: *const u8, value: *co
     let mut _vt = unsafe { &mut * vt };
     let _key = get_array_from_slice_argument(key);
     let _value = get_array_from_slice_argument(value);
-    let _iterator = vec![(_key,_value)];
-    &_vt.trie.insert(_iterator.into_iter());
+    _vt.trie.insert_single(_key,_value);
 }
-//
-// #[no_mangle]
-// pub extern fn verkle_trie_create_path(trie: *mut VerkleTrie<'static>, key: *const u8) -> *mut VerklePath {
-//     let mut _trie = unsafe { &mut * trie};
-//     let _key = Key::from_arr(get_array_from_slice_argument(key));
-//     let _verkle_path = unsafe { transmute( Box::new( _trie.create_verkle_path( &_key ).unwrap() ) ) };
-//     _verkle_path
-// }
-//
-// #[no_mangle]
-// pub extern fn verkle_trie_create_proof(path: *mut VerklePath, keys: *mut SetupKeys) -> *mut VerkleProof {
-//     let mut _path = unsafe { &mut * path};
-//     let mut _keys = unsafe { &mut * keys };
-//     let _commit_key = &_keys.commit_key;
-//     let _verkle_proof = unsafe { transmute( Box::new( _path.create_proof( _commit_key ) ) ) };
-//     _verkle_proof
-// }
-//
-// #[no_mangle]
-// pub extern fn verkle_trie_verify(proof: *mut VerkleProof, path: *mut VerklePath, keys: *mut SetupKeys) -> bool {
-//     let mut _proof = unsafe { &mut * proof};
-//     let mut _path = unsafe { &mut * path};
-//     let mut _keys = unsafe { &mut * keys };
-//     let _opening_key = &_keys.opening_key;
-//     let res = _proof.verify(
-//         _opening_key,
-//         &_path.commitments,
-//         &_path.omega_path_indices,
-//         &_path.node_roots,
-//     );
-//     res
-// }
-//
-//
+
+#[no_mangle]
+pub extern fn get_verkle_proof(vt: *mut VerkleTrie, key: *const u8) -> *mut Proof {
+    let mut _vt = unsafe { &mut *vt};
+    let _key = get_array_from_slice_argument(key);
+    let _proof = _vt.trie.create_verkle_proof(vec![_key].into_iter());
+    let mut proof_bytes = Vec::new();
+    _proof.write(&mut proof_bytes);
+    // println!("{}",bytes.len());
+    // println!("{:?}",bytes);
+    let(_ptr, _len, _) = proof_bytes.into_raw_parts();
+    let mut proof = Proof{ ptr: _ptr, len: _len};
+    unsafe{ transmute( Box::new(proof))}
+}
+
+#[no_mangle]
+pub extern fn verify_verkle_proof(vt: *mut VerkleTrie, ptr: *const u8, proof_len: usize, key: *const u8, value: *const u8) -> bool{
+    let mut proof_bytes = proof_ptr_to_proof_vec(ptr, proof_len);
+    let mut proof = VerkleProof::read(&proof_bytes[..]).unwrap();
+    let mut _vt = unsafe { &mut *vt};
+    let _key = get_array_from_slice_argument(key);
+    let _value = get_array_from_slice_argument(value);
+    let root = _vt.trie.root_commitment();
+    let val_iter = vec![Some(_value)];
+    let vpp = proof.clone();
+    let (res, _) = vpp.check( vec![_key], val_iter, root);
+    return res;
+}
+
+#[no_mangle]
+pub extern fn get_verkle_proof_multiple(vt: *mut VerkleTrie, keys: *const [u8;32], len: usize) -> *mut Proof{
+    let mut _vt = unsafe{ &mut * vt};
+    let _keys = get_vector_from_slice_argument(keys, len);
+    let _proof = _vt.trie.create_verkle_proof(_keys.into_iter());
+    let mut proof_bytes = Vec::new();
+    _proof.write(&mut proof_bytes);
+    let(_ptr, _len, _) = proof_bytes.into_raw_parts();
+    let mut proof = Proof{ ptr: _ptr, len: _len};
+    unsafe{ transmute( Box::new(proof))}
+}
+
+#[no_mangle]
+pub extern fn verify_verkle_proof_multiple(vt: *mut VerkleTrie, ptr: *const u8, proof_len: usize, keys: *const [u8;32], vals: *const [u8;32], len: usize) -> bool{
+    let mut proof_bytes = proof_ptr_to_proof_vec(ptr, proof_len);
+    let mut _vt = unsafe{&mut * vt};
+    let mut proof = VerkleProof::read(&proof_bytes[..]).unwrap();
+    let _keys = get_vector_from_slice_argument(keys, len);
+    let _vals = get_vector_from_slice_argument(vals, len);
+    let root = _vt.trie.root_commitment();
+    let values: Vec<_> = _vals.iter().map(|val| Some(*val)).collect();
+    let vpp = proof.clone();
+    let (res, _) = vpp.check(_keys, values, root);
+    return res;
+}
+#[no_mangle]
+pub extern fn verkle_trie_insert_multiple(vt: *mut VerkleTrie, keys: *const [u8;32], vals: *const [u8;32], len: usize){
+    let mut _vt = unsafe {&mut * vt};
+    let _keys = get_vector_from_slice_argument(keys, len);
+    let _vals = get_vector_from_slice_argument(vals, len);
+    let mut itr = vec![(_keys[0], _vals[0])];
+    for i in 1..=_keys.len() - 1{
+        itr.push((_keys[i], _vals[i]));
+    }
+    _vt.trie.insert(itr.into_iter());
+}
+
+#[no_mangle]
+pub extern fn get_proof_len(_vp: *mut Proof) -> usize{
+    let mut vp = unsafe {&mut *_vp};
+    vp.len
+}
+
+#[no_mangle]
+pub extern fn get_proof_ptr(_vp: *mut Proof) -> *const u8{
+    let mut vp = unsafe {&mut *_vp};
+    vp.ptr
+}
+
 pub fn get_array_from_slice_argument(sl: *const u8) -> [u8; 32] {
     let _raw_slice = unsafe {
         assert!(!sl.is_null());
@@ -104,11 +151,37 @@ pub fn get_array_from_slice_argument(sl: *const u8) -> [u8; 32] {
     _raw_slice.try_into().expect("slice with incorrect length")
 }
 
+pub fn get_vector_from_slice_argument(ptr: *const [u8;32], len: usize) -> Vec<[u8;32]>{
+    assert!(!ptr.is_null());
+    let _raw_slice = unsafe { slice::from_raw_parts(ptr, len)};
+    let mut raw_slice = vec![_raw_slice[0]];
+    for i in 1..= len - 1{
+        raw_slice.push(_raw_slice[i]);
+    }
+    raw_slice
+}
+
+pub fn proof_ptr_to_proof_vec(ptr: *const u8, len:usize) -> Vec<u8>{
+    assert!(!ptr.is_null());
+    let _raw_slice = unsafe { slice::from_raw_parts(ptr, len)};
+    // println!("{:?}",_raw_slice);
+    let mut raw_slice = vec![_raw_slice[0]];
+    for i in 1..= len - 1{
+        raw_slice.push(_raw_slice[i]);
+    }
+    raw_slice
+}
+
 #[cfg(test)]
 mod tests {
     use crate::verkle_trie_new;
     use crate::verkle_trie_insert;
     use crate::verkle_trie_get;
+    use crate::verkle_trie_insert_multiple;
+    use crate::get_verkle_proof_multiple;
+    use crate::verify_verkle_proof_multiple;
+    use crate::get_proof_len;
+    use crate::get_proof_ptr;
     use crate::get_array_from_slice_argument;
     use std::mem::transmute;
 
@@ -200,5 +273,45 @@ mod tests {
         assert!(!val.is_null());
         let val = verkle_trie_get(trie, unsafe {transmute(Box::new(tree_key_code_size))});
         assert!(!val.is_null());
+    }
+
+    #[test]
+    fn generate_proof_test(){
+        let trie = verkle_trie_new();
+
+        let tree_key_version:[u8;32] = [ 121, 85, 7, 198, 131, 230, 143, 90, 165, 129, 173, 81, 186,
+            89, 19, 191, 13, 107, 197, 120, 243, 229, 224, 183, 72, 25, 6, 8, 210, 159, 31, 0];
+
+        let tree_key_balance:[u8;32] = [ 121, 85, 7, 198, 131, 230, 143, 90, 165, 129, 173, 81, 186,
+            89, 19, 191, 13, 107, 197, 120, 243, 229, 224, 183, 72, 25, 6, 8, 210, 159, 31, 1];
+
+        let tree_key_nonce:[u8;32] = [ 121, 85, 7, 198, 131, 230, 143, 90, 165, 129, 173, 81, 186,
+            89, 19, 191, 13, 107, 197, 120, 243, 229, 224, 183, 72, 25, 6, 8, 210, 159, 31, 2];
+
+        let tree_key_code_keccak:[u8;32] = [ 121, 85, 7, 198, 131, 230, 143, 90, 165, 129, 173, 81,
+            186, 89, 19, 191, 13, 107, 197, 120, 243, 229, 224, 183, 72, 25, 6, 8, 210, 159, 31, 3];
+
+        let tree_key_code_size:[u8;32] = [ 121, 85, 7, 198, 131, 230, 143, 90, 165, 129, 173, 81,
+            186, 89, 19, 191, 13, 107, 197, 120, 243, 229, 224, 183, 72, 25, 6, 8, 210, 159, 31, 4];
+        
+        let empty_code_hash_value:[u8;32] = [ 197, 210, 70, 1, 134, 247, 35, 60, 146, 126, 125, 178,
+            220, 199, 3, 192, 229, 0, 182, 83, 202, 130, 39, 59, 123, 250, 216, 4, 93, 133, 164, 112];
+
+        let value_0:[u8;32] = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+        let value_2:[u8;32] = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 2];
+
+        let all_keys = vec![tree_key_version, tree_key_balance, tree_key_nonce, tree_key_code_keccak, tree_key_code_size];
+        let all_vals = vec![value_0, value_2, value_0, empty_code_hash_value, value_0];
+
+        verkle_trie_insert_multiple(trie, all_keys.as_ptr(), all_vals.as_ptr(), all_keys.len());
+
+        let mut multi_proof = get_verkle_proof_multiple(trie, all_keys.as_ptr(), all_keys.len());
+        let proof_ptr = get_proof_ptr(multi_proof);
+        let proof_len = get_proof_len(multi_proof);
+        let verification = verify_verkle_proof_multiple(trie, proof_ptr, proof_len, all_keys.as_ptr(), all_vals.as_ptr(), all_keys.len());
+        assert!(verification);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 // use rust_verkle::{dummy_setup, verkle_trie_new, verkle_trie_insert, verkle_trie_create_path, verkle_trie_create_proof, verkle_trie_verify};
 use rust_verkle::verkle_trie_new;
 use rust_verkle::verkle_trie_insert;
+use rust_verkle::verkle_trie_insert_multiple;
 use rust_verkle::verkle_trie_get;
-use rust_verkle::get_array_from_slice_argument;
+use rust_verkle::get_verkle_proof;
+use rust_verkle::verify_verkle_proof;
+use rust_verkle::{get_verkle_proof_multiple, verify_verkle_proof_multiple};
 use std::mem::transmute;
+use rust_verkle::Proof;
 
 fn main() {
 
@@ -16,7 +20,7 @@ fn main() {
     ];
     let one: *const u8  = unsafe {transmute(Box::new(_one))};
     let _one_32:[u8;32] = [1; 32];
-    let one_32 = unsafe {transmute(Box::new(_one_32))};
+    let one_32: *const u8 = unsafe {transmute(Box::new(_one_32))};
 
     println!("inserting first set of values...");
     verkle_trie_insert(trie, one, one);
@@ -30,4 +34,47 @@ fn main() {
     let result = * _val;
     assert_eq!(result, _one);
 
+    println!("creating proof...");
+    let mut _proof = get_verkle_proof(trie, one_32);
+    let mut proof = unsafe{&mut *_proof};
+
+    println!("verifying proofs...");
+    let mut check: bool = verify_verkle_proof(trie, proof.ptr, proof.len, one_32, one);
+    assert!(check);
+
+    println!("Creating another trie");
+    let trie2 = verkle_trie_new();
+
+    let keys = vec![_one,_one_32];
+    let vals = vec![_one_32,_one];
+    let len = keys.len();
+    let key_ptr = keys.as_ptr();
+    let val_ptr = vals.as_ptr();
+
+    println!("Inserting multiple values");
+    verkle_trie_insert_multiple(trie2, key_ptr, val_ptr, len);
+
+    println!("Checking for inserted values");
+    let val2 = verkle_trie_get(trie2, one);
+    let _val2: Box<[u8;32]> = unsafe { transmute(val2)};
+    let result2 = * _val2;
+    assert_eq!(result2, _one_32);
+
+    println!("Creating proof for multiple Key Vals");
+    let mut _proof2 = get_verkle_proof_multiple(trie2, key_ptr, len);
+    let mut proof2 = unsafe{ &mut *_proof2};
+
+    // println!("verifying proof 1");
+    // check = verify_verkle_proof(trie2, proof2, one_32, one);
+    // assert!(check);
+
+    // println!("verifying proofs 2");
+    // check = verify_verkle_proof(trie2, proof2, one_32, one);
+    // assert!(check);
+
+    println!("verifying multiple proofs");
+    check = verify_verkle_proof_multiple(trie2, proof2.ptr, proof2.len, key_ptr, val_ptr, len);
+    assert!(check);
+
+    println!("All Correct");
 }
